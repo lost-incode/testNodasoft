e5c28aeb941451fee0ef5fd7efc8c961c138ec44
Vicktoria <vicktoriaap@gmail.com>
2022-01-25 17:43:37 +0300

    Init project

---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sourcemap = require("gulp-sourcemaps");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
+const rename = require("gulp-rename");
+const htmlmin = require("gulp-htmlmin");
+const del = require("del");
+const sync = require("browser-sync").create();
+
+// Styles
+
+const styles = () => {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
+}
+
+exports.styles = styles;
+
+// HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest("build"));
+}
+
+exports.html = html;
+
+// Clean
+
+const clean = () => {
+  return del(["build/*/"])
+}
+
+exports.clean = clean;
+
+//Copy
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+  }
+
+  exports.copy = copy;
+
+// Build
+
+const build = gulp.series(
+  clean,
+  copy,
+  gulp.parallel(
+    styles,
+    html,
+  ),
+);
+
+exports.build = build;
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+exports.server = server;
+
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
+
+// Watcher
+
+const watcher = () => {
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/*.html", gulp.series(html, reload));
+}
+
+//Default
+
+exports.default = gulp.series(
+  clean,
+  copy,
+  gulp.parallel(
+    styles,
+    html,
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
+);
